7e6d8ffa10494c718a1e8e2b3bc1a5243e6eb6b5
Bettadapura S. Sharma <bsrsharma@yahoo.com>
2018-05-17 21:24:57 -0700

    receive data  and save it in a database

---
+++ record.c
@@ -0,0 +1,156 @@
+#define PORT 53556
+#define SERVER_ADDRESS  "192.168.0.101"
+#define BUFLEN 64
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "sqlite3.h"
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    int i;
+    for(i=0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+
+main(argc, argv)
+{
+   printf("Event Recording System\n");
+
+
+   int serverfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+
+   if (serverfd < 0) {
+           puts("socket() failed");
+           exit(-1);
+   }
+
+   struct sockaddr_in server_addr, client_addr ;
+
+   memset(&server_addr, 0, sizeof(struct sockaddr_in));
+
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = htons(PORT);
+
+   if (inet_aton(SERVER_ADDRESS, &server_addr.sin_addr)==0) {
+       puts("inet_aton() failed");
+       exit(-1);
+   }
+
+   if (bind(serverfd, (const struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) == -1)  {
+       printf("bind() failed; errno = %d\n", errno);
+       exit(-1);
+   }
+
+// Open the report Database
+
+   /* Database open API
+
+   int sqlite3_open(
+      const char *filename,   // Database filename (UTF-8) 
+      sqlite3 **ppDb          // OUT: SQLite db handle
+   );
+   */
+
+   sqlite3 *pDb;
+   int rc;
+   char *zErrMsg = 0;
+   char sqlInsertCmd[256] = "INSERT INTO DEVICE_REPORT VALUES(65535,255,9999999999);";
+   char devRec[64] = "65535,255,9999999999";
+
+   rc = sqlite3_open("DeviceDB", &pDb);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(pDb));
+      sqlite3_close(pDb);
+      return(-1);
+   }
+
+  /* Database exec API
+
+  int sqlite3_exec(
+     sqlite3*,                                  // An open database
+     const char *sql,                           // SQL to be evaluated
+     int (*callback)(void*,int,char**,char**),  // Callback function 
+     void *,                                    // 1st argument to callback
+     char **errmsg                              // Error msg written here
+  );
+  */
+
+
+   rc = sqlite3_exec(pDb,
+        "CREATE TABLE IF NOT EXISTS DEVICE_REPORT(ID INT NOT NULL,OP INT NOT NULL,TIME INT NOT NULL);",
+         NULL, 0, &zErrMsg);
+   if( rc!=SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }
+
+   uint8_t buf[BUFLEN], recvlen;
+   int slen = sizeof(struct sockaddr_in);
+
+   /*
+   For testing, run count times
+   For normal use, run continuously as a background process or daemon
+
+   int count;
+
+   for (count = 0; count < 100; count++ ) {
+   */
+
+   for(;;) {    
+      if ((recvlen = recvfrom(serverfd, buf, BUFLEN, 0,
+           (struct sockaddr * __restrict__)&client_addr,
+           &slen)) < 0)
+      {
+           puts("recvfrom() failed");
+           exit(-1);
+      }
+
+/*   
+      printf("Received packet from %s:%d\n", 
+               inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port));
+      int i;                          
+      for(i=0; i < recvlen; i++)
+         printf("%02X ", buf[i]);
+      printf("\n\n"); 
+*/
+
+      printf("Received devid = %d, op = %d, time = %d\n",
+      *(uint16_t *)buf, *(uint8_t *)(buf+2), *(uint32_t *)(buf+3) );
+
+      // insert report into Database
+
+     sprintf(sqlInsertCmd,
+     "INSERT INTO DEVICE_REPORT VALUES(%d,%d,%d);",
+     *(uint16_t *)buf, *(uint8_t *)(buf+2), *(uint32_t *)(buf+3)
+     );
+
+     rc = sqlite3_exec(pDb, sqlInsertCmd, NULL, 0, &zErrMsg);
+     if( rc!=SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+     }
+
+   } // count
+
+
+   // Close Database
+
+   sqlite3_close(pDb);
+
+}
